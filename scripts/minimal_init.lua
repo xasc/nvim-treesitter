ba14aff17c37018feaafb548ab1deed9c9dafe08
Christoph Sax <christoph.sax@mailbox.org>
2023-07-24 22:10:13 +0200

    Support nodes for HLL extension

--- scripts/minimal_init.lua
+++ scripts/minimal_init.lua
@@ -27,3 +27,17 @@ vim.api.nvim_create_autocmd('FileType', {
     vim.bo[args.buf].indentexpr = 'v:lua.require"nvim-treesitter".indentexpr()'
   end,
 })
+--
+-- local parser_config = require("nvim-treesitter.parsers").get_parser_configs()
+-- parser_config.t32 = {
+--   install_info = {
+--     -- url = "/home/christoph/workspace/tree-sitter-t32", -- local path or git repo
+--     url = "https://codeberg.org/xasc/tree-sitter-t32", -- local path or git repo
+--     files = { "src/parser.c", "src/scanner.c" },
+--     -- optional entries:
+--     branch = "main", -- default branch in case of git repo if different from master
+--     generate_requires_npm = false, -- if stand-alone parser without npm dependencies
+--     requires_generate_from_grammar = false, -- if folder contains pre-generated src/parser.c
+--   },
+--   filetype = "t32", -- if filetype does not match the parser name
+-- }
